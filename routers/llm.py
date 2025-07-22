3fa53d09030aebe562efa7081cf9a845bec310d6
Vinithavn <vinithavn01@gmail.com>
2025-07-22 13:17:33 +0530

    Added files

---
+++ routers/llm.py
@@ -0,0 +1,187 @@
+from fastapi import APIRouter,File,UploadFile,Query,Depends,HTTPException
+from datetime import datetime
+
+
+from llm_utils import retrieve_generate, summarizer
+from typing import Optional,Annotated
+from llm_utils.completion import generate
+from utils import *
+from database import session_local
+from routers.auth import get_current_user
+from sqlalchemy.orm import Session
+from starlette import status
+from models import ChatHistory
+import shutil
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from warnings import filterwarnings
+filterwarnings("ignore")
+
+load_dotenv()
+
+router = APIRouter(prefix = "/llm",tags = ["llm"])
+
+allowed_extensions = [".pdf",".jpg",".png",".jpeg",".html",".docx"]
+
+conn_url = os.getenv('MONGO_URI')
+if conn_url is None:
+    conn_url = "mongodb://localhost:27017/"
+print(conn_url)
+
+try:
+    client = MongoClient(conn_url)
+    db = client["your_database_name"]  # Choose your database name
+    collection = db["chat_history"]  # Choose your collection name
+    collection.create_index([("user_id", 1)]) #Creating an index for user_id for faster search. 1 indicate the sort in ascending order
+    print("Successfully connected to MongoDB.")
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+
+
+def get_db():
+    db = session_local() # Returns a new database session
+    try:
+        yield db # Yield will pause the function execution here, returning the db object to the caller
+    finally:
+        db.close() # Close the connection after each session
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict,Depends(get_current_user)]
+
+def upload_document(file):
+    file_extension = "." + file.filename.split(".")[-1]
+    if file_extension not in allowed_extensions:
+        return "File format not supported"
+    else:
+        if not os.path.exists("input_data_files"):
+            os.makedirs("input_data_files")
+        else:
+            shutil.rmtree("input_data_files")
+            os.makedirs("input_data_files")
+        temp_file_path = f'input_data_files/temp_{file.filename}'
+        try:
+            with open(temp_file_path,'wb') as f:
+                shutil.copyfileobj(file.file,f)
+            upload = True
+        except:
+            upload = False
+        return upload
+
+    
+        
+@router.post("/ask-from-document")
+def ask_from_document(db:db_dependency,
+                      user:user_dependency,
+                      query:str,
+                      model_name: str = Query('Gemini', enum=['Gemini', 'Lllama 3.2']),
+                      file:UploadFile=File(...)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+
+    upload = upload_document(file)
+    if not upload:
+        return "Unable to upload"
+    else:
+        doc_path = os.listdir("input_data_files")
+        complete_path = ["input_data_files/"+i for i in doc_path]
+        user_id = user.get('id')
+        chat_history_model = db.query(ChatHistory).filter(ChatHistory.user_id == user_id).first()
+
+        if chat_history_model is None:
+            print("New user")
+        else:
+            print("Existing User")
+
+        chat_message_history = get_chat_history(collection, user_id)
+        llm_response = retrieve_generate.ask_llm(query, chat_message_history,complete_path, model_name)
+        chat_history_model_new = ChatHistory(
+            user_id=user_id,
+            chat_title=f"{user_id}_chat")
+        db.add(chat_history_model_new)
+        db.commit()
+
+        # Store the updated chat history to Mongodb
+        try:
+            print("Adding the user query to collection")
+            save_chat_history(collection, user_id, query, llm_response)
+        except:
+            raise HTTPException(status_code=500, detail="An error occurred during chat.")
+        return llm_response 
+    
+
+@router.post("/summarize")
+async def summarize(db:db_dependency,
+                    user:user_dependency,
+                    model_name: str = Query('Gemini', enum=['Gemini', 'Lllama 3.2']),
+                    context:Optional[str]=None,
+                    file:Optional[UploadFile] = File(None)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+
+    if not file:
+        if context is not None:
+            llm_response = summarizer.summarize_text(context, model_name)
+            return llm_response
+        else:
+            return "Either upload a document or provide text to summarize"
+    
+    else:
+        upload = upload_document(file)
+        if not upload:
+            return "Unable to upload"
+        else:
+            doc_path = os.listdir("input_data_files")
+            complete_path = ["input_data_files/"+i for i in doc_path]
+            llm_response = summarizer.summarize_document(complete_path, model_name)
+            return llm_response
+        
+@router.post("/chat")
+async def chat(db:db_dependency,
+               user:user_dependency,
+               query:str,
+               model_name:str=Query('Gemini',enum=["Gemini",'Llama 3.2'])):
+
+    # If the user fail to authenticate
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+
+    # If the user is an authenticated user
+    user_id = user.get("id")
+    chat_history_model = db.query(ChatHistory).filter(ChatHistory.user_id == user_id).first()
+
+    if chat_history_model is None:
+        print("New user")
+    else:
+        print("Existing User")
+
+
+    chat_message_history = get_chat_history(collection,user_id)
+
+    print("CHAT HISTORY:",chat_message_history)
+    print("User query:",query)
+
+    # Get the LLM response for the query with history
+    
+    llm_response = generate(chat_message_history,query,model_name)
+    #store the chat message information to the sqlite db
+
+    chat_history_model_new = ChatHistory(
+        user_id=user_id,
+        chat_title=f"{user_id}_chat")
+    db.add(chat_history_model_new)
+    db.commit()
+
+    #Store the updated chat history to Mongodb
+    try:
+        print("Adding the user query to collection")
+        save_chat_history(collection,user_id,query,llm_response)
+    except:
+       raise HTTPException(status_code=500, detail="An error occurred during chat.")
+
+    return llm_response
+
+
+
+    
---
+++ utils.py
@@ -0,0 +1,120 @@
+from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
+from langchain_ollama import ChatOllama,OllamaEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from unstructured.partition.auto import partition
+from langchain_core.documents import Document 
+import shutil
+import os
+from datetime import datetime
+
+
+
+allowed_extensions = [".pdf",".jpg",".png",".jpeg",".html",".docx"] 
+
+partition_function_dict = {
+    ".csv":"partition_csv",    
+    ".eml":"partition_email",     
+    ".msg":"partition_msg",    
+    ".epub":"partition_epub",    
+    ".xls":"partition_xlsx",    
+    ".xlsx":"partition_xlsx",    
+    ".html":"partition_html",    
+    ".htm":"partition_html",    
+    ".bmp":"partition_image",    
+    ".heic":"partition_image",    
+    ".jpeg":"partition_image",     
+    ".png":"partition_image",     
+    ".jpg":"partition_image",    
+    ".tiff":"partition_image",    
+    ".md":"partition_md",    
+    ".org":"partition_org",    
+    ".odt":"partition_odt",    
+    ".pdf":"partition_pdf",    
+    ".txt":"partition_text",    
+    ".text":"partition_text",    
+    ".log":"partition_text",    
+    ".ppt":"partition_ppt",     
+    ".pptx":"partition_pptx",    
+    ".rst":"partition_rst",    
+    ".rtf":"partition_rtf",    
+    ".tsv":"partition_tsv",    
+    ".doc":"partition_doc",     
+    ".docx":"partition_docx",    
+    ".xml":"partition_xml" } 
+
+def extract_text_all_formats(filenames:list[str]):  
+    documents = []  
+    i = 0 
+    for filename in filenames:    
+        extension = "."+filename.split(".")[1]    
+        if extension in partition_function_dict.keys():      
+            partition_function = partition_function_dict[extension]    
+        else:      
+            partition_function=None    
+        elements = partition(filename=filename, content_type=partition_function)    
+        extracted_text = "\n\n".join([str(el) for el in elements])    
+        documents.append(Document(id=i, metadata={"source": filename},page_content=extracted_text,))    
+        i+=1 
+    return documents
+
+def upload_document(file):
+    file_extension = "." + file.filename.split(".")[-1]
+    if file_extension not in allowed_extensions:
+        return "File format not suppported"
+    else:
+        if not os.path.exists("input_data_files"):
+            os.makedirs("input_data_files")
+        else:
+            shutil.rmtree("input_data_files")
+        temp_file_path = f'LLM_App/input_data_files/temp_{file.filename}'
+        try:
+            with open(temp_file_path,'wb') as f:
+                shutil.copyfileobj(file.file,f)
+            return {"status":"Document uploaded successfully."}
+        except:
+            return {"status":"Unable to upload the document."}
+
+def load_model(model_name):
+    if model_name == "Lllama 3.2":
+        llm = ChatOllama(
+        model="llama3.2",
+        temperature=0,
+        )
+        embeddings = OllamaEmbeddings(model="llama3.2")
+    else:
+        model_name = "Gemini"
+        llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    return llm,embeddings
+
+#read the document from the user 
+def read_document(doc_path):
+    loader = PyPDFLoader(doc_path)
+    return loader.load()
+
+def get_chat_history(collection,user_id):
+    history_collection = list(collection.find(
+        {"user_id": user_id},
+        sort=[("timestamp", -1)],  # Sort by timestamp, newest first
+        limit=10
+    ))
+    if history_collection:
+        history_collection.reverse()
+        chat_message_history = " ".join([chat["role"] + ":" + chat["content"] + "\n" for chat in history_collection])
+    else:
+        chat_message_history = None
+    return chat_message_history
+
+def save_chat_history(collection,user_id,query,llm_response):
+    collection.insert_one({
+        "user_id": user_id,
+        "timestamp": datetime.now(),
+        "role": "user",
+        "content": query
+    })
+    collection.insert_one({
+        "user_id": user_id,
+        "timestamp": datetime.now(),
+        "role": "assistant",
+        "content": llm_response  # replace with actual response.
+    })
